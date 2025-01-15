df96403b7c4959a4b43b87b656d89caa79511021
Cam Race <cameron.race@education.gov.uk>
2025-01-15 21:50:07 +0000

    version 1.0.1

--- inst/doc/connecting_to_sql.R
+++ inst/doc/connecting_to_sql.R
@@ -5,37 +5,37 @@ knitr::opts_chunk$set(
 )
 
 ## ----local connection, eval=FALSE---------------------------------------------
-# # Library calls ====
-# 
-# library(odbc)
-# library(DBI)
-# 
-# # Database connection ====
-# 
-# con <- DBI::dbConnect(odbc::odbc(),
-#   Driver = "ODBC Driver 17 for SQL Server",
-#   Server = "server_name",
-#   Database = "database_name",
-#   UID = "",
-#   PWD = "",
-#   Trusted_Connection = "Yes"
-# )
+#  # Library calls ====
+#  
+#  library(odbc)
+#  library(DBI)
+#  
+#  # Database connection ====
+#  
+#  con <- DBI::dbConnect(odbc::odbc(),
+#    Driver = "ODBC Driver 17 for SQL Server",
+#    Server = "server_name",
+#    Database = "database_name",
+#    UID = "",
+#    PWD = "",
+#    Trusted_Connection = "Yes"
+#  )
 
 ## ----reading clean sql, eval=FALSE--------------------------------------------
-# sql_query <- dfeR::get_clean_sql("path_to_sql_file.sql")
+#  sql_query <- dfeR::get_clean_sql("path_to_sql_file.sql")
 
 ## ----executing sql query, eval=FALSE------------------------------------------
-# sql_query_result <- DBI::dbGetQuery(con, statement = sql_query)
+#  sql_query_result <- DBI::dbGetQuery(con, statement = sql_query)
 
 ## ----executing sql query inline, eval=FALSE-----------------------------------
-# sql_query_result <- DBI::dbGetQuery(
-#   con,
-#   statement = "SELECT * FROM [my_database_table]"
-# )
+#  sql_query_result <- DBI::dbGetQuery(
+#    con,
+#    statement = "SELECT * FROM [my_database_table]"
+#  )
 
 ## ----reading clean sql additional, eval=FALSE---------------------------------
-# sql_query <- dfeR::get_clean_sql(
-#   "path_to_sql_file.sql",
-#   additional_settings = TRUE
-# )
+#  sql_query <- dfeR::get_clean_sql(
+#    "path_to_sql_file.sql",
+#    additional_settings = TRUE
+#  )
 
